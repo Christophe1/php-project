b155c22c8384ecc4d3b3d68faff159ab8fb5b991
Christophe1 <harris.christophe@gmail.com>
2017-05-04 00:31:34 +0100

    checkcontact.php working as it should, inserted correctly into contacts table, no duplicates.

--- checkcontact.php
+++ checkcontact.php
@@ -6,103 +6,60 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //***************************************************
 require('dbConnect.php');
-
-
-//this is me, +567890123, my user_id in the user table
+//this is me, my user_id in the user table
 $user_id = 20;
-
 //post all contacts in my phone as a JSON array
 $json = $_POST['phonenumber'];
 //decode the JSON
 $array = json_decode($json);
-//bind 
+//bind. We want to check if contacts in my phone are also users of the app. 
+//if they are, then we want to put those phone contacts into the contacts table, as friends of user_id = 20.
  $query = "SELECT * FROM user WHERE username = ?";
  $stmt = $con->prepare($query) or die(mysqli_error($con));
  $stmt->bind_param('s', $phonenumber) or die ("MySQLi-stmt binding failed ".$stmt->error);
-
-
  //for each value of phone_number in the array, call it $phonenumber
 	foreach ($array as $value)
 	{
 		$phonenumber = $value->phone_number;
 		
-			//	$sql = "SELECT * FROM user WHERE username = '$phonenumber'";
-		//$result = mysqli_query($con, $sql);
-	
-		//*****SECURE
 $stmt->execute() or die ("MySQLi-stmt execute failed ".$stmt->error);
-//$result = $con->($query);
-//$result = $stmt->get_result(); 
-//$stmt->store_result();
-//$num_rows = $stmt->num_rows();
- //$result = mysqli_query($con, $query);
-//echo $result;
- //if ($stmt->num_rows > 0) { // Check if there are any rows matching this value
+//store the result of contacts using the app
 	 $result = $stmt->get_result(); // Convert from MySQLi_stmt to MySQLi_result (to use fetch_assoc())
-	//  while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-	// while($row = mysqli_fetch_assoc($result)){ 
+
 	     echo "Number of rows matching username '".$value->phone_number."' from user-table is " . $result->num_rows  . " rows.<br>"; 
-	// $contact_id = $row['user_id'];
-	// echo $contact_id;
-	//	echo "phonenumber is " . $phonenumber  . "<br>";
-	//echo "number of phone numbers in the user table is " . $result->num_rows  . "<br>"; 
+
 	        while ($row = $result->fetch_assoc()) {
 			//this the the user_id in the user table of the matching phone number	
             echo $row['user_id']."<br />";
 			//call this user_id contact_id
 			$contact_id = $row['user_id'];
+			echo $contact_id;
+
+				//make a select statement for contacts table where user_id and contact_id = $user_id and $contact_id.If
+				//this value doesn't exist then put it in the contacts table
+				
+				$query3 = "SELECT * FROM contacts WHERE user_id = ? AND contact_id = ?";
+				$stmt3 = $con->prepare($query3) or die(mysqli_error($con));
+				$stmt3->bind_param('ii', $user_id, $contact_id) or die ("MySQLi-stmt binding failed ".$stmt3->error);
+				$stmt3->execute() or die ("MySQLi-stmt execute failed ".$stmt3->error);
+			    $result3 = $stmt3->get_result();
 			
-			//if ($stmt->num_rows == 0)
-			
+			   // echo "test to see if num_rows is correct : " . $result3->num_rows;
+
+			   //if the contact is not already in the contacts table, then put him in
+			    If ($result3->num_rows == 0) {
 				$stmt2 = $con->prepare("INSERT INTO contacts (user_id, contact_id) VALUES(?,?)") or die(mysqli_error($con));
 				$stmt2->bind_param('ii', $user_id, $contact_id) or die ("MySQLi-stmt binding failed ".$stmt2->error);
 				$stmt2->execute() or die ("MySQLi-stmt execute failed ".$stmt2->error);
-	//$insert_into_contacts_table = mysqli_query($con,$insert_into_contacts_command);
-			
-	} //}
-/* 	      else {
-        echo "No rows matching username ".$value->phone_number.".<br />";
-    } */
+				}
+
+	} 
+
  }
-// while ($row = $stmt->fetch_array(MYSQLI_ASSOC)) {
-    // 
 
-//}
- 
- /* Bind results */
-   // $stmt -> bind_result($testfield1);
-
-    /* Fetch the value */
-  //  $stmt -> fetch();
-
-// $num_rows = $stmt->num_rows;
-//*****
-//echo "phonenumber is " . $phonenumber  . "<br>";
-
-	
-//	if($num_rows > 0) {
-
-	//echo "phonenumber is " . $phonenumber  . "<br>";
- 
-//echo $phonenumber . "<br>";
-//var_dump($result);
-//var_dump($stmt);
-//$num_rows = $stmt->num_rows();
-//*****
-/* 	}
-	else {
-		echo "no match";
-	} */
-//	}
-//	echo $num_rows . "<br>";
 var_dump($_POST["phonenumber"]);
 $stmt->close();
-//mysqli_close($con);	
-/* 	
-	else {
-	var_dump($stmt);
-	
-	
-	} */
+$stmt2->close();
+$stmt3->close();
 		
 		?>
