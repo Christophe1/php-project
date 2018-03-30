f2131804f7c4183e47ca8a10e70689359b240790
Christophe1 <harris.christophe@gmail.com>
2018-03-30 22:21:01 +0100

    adding CategorySearch.php

--- AllCategories.php
+++ AllCategories.php
@@ -2,29 +2,41 @@
 
 require('dbConnect.php');
 
-$sql = "SELECT cat_name FROM category";
+//this is me, my username in the user table
+//$Number = $_POST['phonenumberofuser'];
+$Number = "+353872934480";
 
-$result = $con->query($sql);
+// The ? below are parameter markers used for variable binding
+// auto increment does not need prepared statements
 
-if ($result->num_rows >0) {
- 
- 
- while($row[] = $result->fetch_assoc()) {
- 
- $tem = $row;
- 
- $json = json_encode($tem);
- 
- 
- }
- 
-} else {
- echo "No Results Found.";
-}
- echo $json;
-$con->close();
+				$query = "SELECT * FROM user WHERE username = ?";
+				$stmt = $con->prepare($query) or die(mysqli_error($con));
+				$stmt->bind_param('s', $Number) or die ("MySQLi-stmt binding failed ".$stmt->error);
+				$stmt->execute() or die ("MySQLi-stmt execute failed ".$stmt->error);
+			    $result = $stmt->get_result();
+				
+			while ($row = $result->fetch_assoc()) {
+				//get the corresponding user_id in the row
+			$user_id = $row["user_id"];
+			
+			//here is the user_id, which is the corresponding user_id for username Joe Blogs
+			//echo $user_id;
+			}
 
+$sql = "SELECT cat_name FROM category WHERE user_id = ?";
 
+$stmt2 = $con->prepare($sql) or die(mysqli_error($con));
+$stmt2->bind_param('i', $user_id) or die ("MySQLi-stmt binding failed ".$stmt2->error);
+$stmt2->execute() or die ("MySQLi-stmt execute failed ".$stmt2->error);
+$result2 = $stmt2->get_result();
 
+while ($row[] = $result2->fetch_assoc()) {
+ $data = $row;
+ 
+ $json = json_encode($data);
 
+} 
+	
+	echo $json;
+	
 ?>
