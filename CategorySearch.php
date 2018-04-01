500f15eb1dc0e3a1a0b20010fe0b88f7dc3763d4
Christophe1 <harris.christophe@gmail.com>
2018-04-01 23:21:08 +0100

    Added a 'count' for private review ids in CategorySearch

--- CategorySearch.php
+++ CategorySearch.php
@@ -2,6 +2,30 @@
 
 require('dbConnect.php');
 
+//this is me, my username in the user table
+//$Number = $_POST['phonenumberofuser'];
+//$Number = "+353872934480";
+/* $Number = "+353864677745";
+
+// The ? below are parameter markers used for variable binding
+// auto increment does not need prepared statements
+
+			//Select all related info in the user table for the user +353864677745, or whatever
+			$query = "SELECT * FROM user WHERE username = ?";
+			$stmt = $con->prepare($query) or die(mysqli_error($con));
+			$stmt->bind_param('s', $Number) or die ("MySQLi-stmt binding failed ".$stmt->error);
+			$stmt->execute() or die ("MySQLi-stmt execute failed ".$stmt->error);
+			$result = $stmt->get_result();
+				
+				//fetch all rows associated with the value +353864677745
+				while ($row = $result->fetch_assoc()) {
+				//for the user_id row associated with user_name +353864677745, call it $user_id
+				$user_id = $row["user_id"];
+			
+				//here is the user_id, which is the corresponding user_id for username +353864677745
+				echo $user_id;
+				} */
+
 			$user_id = "21";
 
 			//Select all related info in the review_shared table 
@@ -30,14 +54,19 @@ require('dbConnect.php');
 				//make an array called $results
 				$results[$row['cat_id']][] = $review_id; 
 				
+				//echo count($results[$row['cat_id']]) . ",";
+				
 				}
 					
 			$jsonData = array_map(function($catId) use ($results) {
 			return [
 					'category' => $catId,
+					'count' => count($results[$catId]),
 					'review_ids' => $results[$catId]
+					
 					];
 			}, array_keys($results));
 
 			echo json_encode($jsonData);
+			
 ?>
